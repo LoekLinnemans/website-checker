b019483fc37bc4f580ee898e10ce3168543639a6
Loek Linnemans <144667567+LoekLinnemans@users.noreply.github.com>
2024-09-09 14:33:24 +0200

    add a basic fucntion to test

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	var url string
+	fmt.Print("Enter the Website: ")
+	fmt.Scan(&url)
+	resp, err := http.Get(url)
+	fmt.Print(resp)
+	fmt.Print(err)
 }
