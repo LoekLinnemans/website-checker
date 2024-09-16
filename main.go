de884fb17149bef900fa7080173343ee8b3b4efb
Loek Linnemans <144667567+LoekLinnemans@users.noreply.github.com>
2024-09-16 13:52:06 +0200

    added logging file logic

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
+
+func init() {
+	logFile, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Println("couldn't create logfile", err)
+		os.Exit(1)
+	}
+
+	writer := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(writer)
+
+}
 
 func main() {
 	var url string
 	fmt.Print("Enter the Website: ")
 	fmt.Scan(&url)
 	resp, err := http.Get(url)
-	fmt.Print(resp)
-	fmt.Print(err)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(resp)
 }
